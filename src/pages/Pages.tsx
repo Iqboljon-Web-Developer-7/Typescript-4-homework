e10a4ae7091ed53f69c3c35e72d4bf99baafc38c
Iqboljon <iqboljonummataliyev30@gmail.com>
2024-10-03 15:19:25 +0500

    updated

--- src/pages/Pages.tsx
+++ src/pages/Pages.tsx
@@ -1,4 +1,4 @@
-import React, { Component } from "react";
+import { Component } from "react";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import Products from "../components/products/Products";
 import Product from "../components/product/Product";
